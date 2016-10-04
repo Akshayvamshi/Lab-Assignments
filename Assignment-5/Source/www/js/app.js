d1d6086bb98026a9085fe1a6d30429ca42ce7398
rajendraprasadpalagarla <rajendrapalagarla1994@gmail.com>
2016-10-04 14:07:34 -0600

    updated

--- Assignment-5/Source/www/js/app.js
+++ Assignment-5/Source/www/js/app.js
@@ -1,4 +1,26 @@
-angular.module('starter', ['ionic'])
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
 
   .controller('AppCtrl', function($scope) {
     $scope.data = {
@@ -6,7 +28,7 @@ angular.module('starter', ['ionic'])
     };
     $scope.recognizedText = '';
 
-    $scope.speakText = function() {
+    $scope.speakText = function () {
       TTS.speak({
         text: $scope.data.speechText,
         locale: 'en-GB',
@@ -17,15 +39,5 @@ angular.module('starter', ['ionic'])
         // Handle the error case
       });
     };
-
-    $scope.record = function() {
-      var recognition = new SpeechRecognition();
-      recognition.onresult = function(event) {
-        if (event.results.length > 0) {
-          $scope.recognizedText = event.results[0][0].transcript;
-          $scope.$apply()
-        }
-      };
-      recognition.start();
-    };
   });
+
